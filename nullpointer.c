6d5438bf23c6f3bcf70b509d3902a3dced5b7b86
PRATHAMTEJANI <prathamtejani4@gmail.com>
2024-12-13 10:08:02 +0530

    Add files via upload
    null pointer in c language

---
+++ nullpointer.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    /* int ptr = 10;
+    
+    if(ptr == NULL)
+    {
+        printf("pointer does not to print anything");
+    }
+    else{
+        printf("a value pointed by pointer :=%d" , ptr);
+
+    } */
+   int *ptr = NULL;
+   printf("%d" , *ptr);
+   
+    return 0;
+}
